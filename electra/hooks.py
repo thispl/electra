ff26cadefbf6ce95d7a62b1c88aa4cd92ccdedf3
Kartheek <kartheek@valiantsystems.com>
2022-05-16 16:46:05 +0200

    log

--- electra/hooks.py
+++ electra/hooks.py
@@ -15,7 +15,9 @@ app_license = "MIT"
 # include js, css files in header of desk.html
 app_include_css = "/assets/electra/css/electra.css"
 # app_include_js = "/assets/electra/js/electra.js"
-
+app_include_js = [
+            "/assets/electra/js/linkselector.js",
+                ]
 # include js, css files in header of web template
 # web_include_css = "/assets/electra/css/electra.css"
 # web_include_js = "/assets/electra/js/electra.js"
