7c4b270474bd0dfe6f5d658bf60f34b8d68074b9
Kartheek <kartheek@valiantsystems.com>
2022-05-18 09:32:00 +0200

    log

--- electra/public/js/linkselector.js
+++ electra/public/js/linkselector.js
@@ -99,7 +99,9 @@ frappe.ui.form.LinkSelector.prototype.search = function() {
 			start: this.start
 		};
 			console.log("args----")
-console.log(args)
+			console.log(args)
+		args.filters={"name": this.dialog.fields_dict.name.get_value(),"description": this.dialog.fields_dict.description.get_value()}
+		console.log(args)
 	}else{
 		var args = {
 			txt: this.dialog.fields_dict.txt.get_value(),
