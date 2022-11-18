b5107046788133dc7ec7bde2b160e7af8a773e45
Ubuntu <ubuntu@ip-172-31-40-96.me-south-1.compute.internal>
2022-11-18 15:53:13 +0000

    oved wrong files to electra

---
+++ electra/electra/doctype/update_selling_price/update_selling_price.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Abdulla and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Update Selling Price', {
+	// refresh: function(frm) {
+
+	// }
+});
